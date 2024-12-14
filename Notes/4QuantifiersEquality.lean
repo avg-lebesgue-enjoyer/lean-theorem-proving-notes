bd494cf0fd883708e1c79e0edafc375f44fe8293
Gabe Field <gabriel.field@uqconnect.edu.au>
2024-12-14 23:52:12 +1000

    shitpost

--- Notes/4QuantifiersEquality.lean
+++ Notes/4QuantifiersEquality.lean
@@ -5,5 +5,13 @@
 /- SECTION: ∀ -/
 section universal_quantifier
   -- We can encode `∀ x : α, p x` by the dependent function type `(x : α) → p x`. This is obvious if you think for a couple of seconds about it.
-  -- Introduction rule is lambda abstraction; Elimination rule is application
+  -- Introduction rule is lambda abstraction; Elimination rule is application.
+  example (α : Type) (p q : α → Prop) : (∀ x : α, p x ∧ q x) → ∀ y : α, p y :=
+    fun h : (∀ x : α, p x ∧ q x) =>
+    fun y : α =>  -- introduction rule used in `fun y : α => ⋯`
+    (h y).left    -- elimination rule used in application `h y`
 end universal_quantifier
+
+
+
+/- SECTION: = -/
