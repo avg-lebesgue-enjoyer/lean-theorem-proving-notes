e5f8f6c6e2c3abaedf3f2e6e89d131d82eaee9f5
Gabe Field <gabriel.field@uqconnect.edu.au>
2024-12-16 16:06:35 +1000

    shitpost

--- Notes/5Tactics.lean
+++ Notes/5Tactics.lean
@@ -1,3 +1,67 @@
 /- @file Notes/5Tactics.lean
  - @src https://lean-lang.org/theorem_proving_in_lean4/tactics.html
  -/
+
+/- SECTION: Entering Tactic Mode -/
+section entering
+  -- NOTE: `apply`
+  theorem test
+    (p q : Prop)
+    (hp : p) (hq : q)
+    : p ∧ q ∧ p
+    := by
+      apply And.intro -- `apply f` will take an implication `f`, whose conclusion is the same type as the current goal, and replace this goal by separate goals for all arguments of `f`
+      · exact hp      -- `exact` is a synonym for `apply`, to be used when there are no arguments
+      · apply And.intro
+        · exact hq
+        · exact hp
+  #print test
+  -- Another way to use `apply`
+  theorem test'
+    (p q : Prop)
+    (hp : p) (hq : q)
+    : p ∧ q ∧ p
+    := by
+      apply And.intro hp
+      apply And.intro hq hp
+  -- Can use a semicolon to separate tactics on a single line
+  theorem test''
+    (p q : Prop)
+    (hp : p) (hq : q)
+    : p ∧ q ∧ p
+    := by
+      apply And.intro hp ; apply And.intro hq hp
+  -- You can switch between different cases using the `case ⋯ => ⋯` construct
+  theorem test'''
+    (p q : Prop)
+    (hp : p) (hq : q)
+    : p ∧ q ∧ p
+    := by
+      apply And.intro
+      case right => -- they don't need to be in order...!
+        apply And.intro
+        case left  => exact hq
+        case right => exact hp
+      case left =>
+        exact hp
+  -- `·` (`\.` or just `.`) is a shorthand for a `case ⋯ => ⋯` expression which matches the first outstanding goal
+  theorem test''''
+    (p q : Prop)
+    (hp : p) (hq : q)
+    : p ∧ q ∧ p
+    := by
+      apply And.intro
+      case right =>
+        apply And.intro
+        · exact hq
+        · exact hp
+      case left =>
+        exact hp
+end entering
+
+
+
+/- SECTION: Basic Tactics -/
+section basic_tactics
+  -- Shitpost
+end basic_tactics
