87b240757b4d829495c0a4fb2b39af0e8793c2ef
Gabe Field <gabriel.field@uqconnect.edu.au>
2024-12-19 00:32:57 +1000

    shitpost

---
+++ Notes/8InductionRecursion.lean
@@ -0,0 +1,12 @@
+/- @file Notes/8InductionRecursion.lean
+ - @src https://lean-lang.org/theorem_proving_in_lean4/induction_and_recursion.html
+ -/
+
+/- SECTION: Pattern Matching -/
+-- Yeah it exists
+
+
+
+/- SECTION: Wildcards and Overlapping Patterns -/
+-- NOTE: Lean handles overlapping patterns the same way that Haskell does;
+--        take the first match
