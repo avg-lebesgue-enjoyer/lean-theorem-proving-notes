e93e6d6cfc1274f6a2284c40b9fc3451855f3c07
Gabe Field <gabriel.field@uqconnect.edu.au>
2024-12-19 10:44:37 +1000

    shitpost

--- Notes/8InductionRecursion.lean
+++ Notes/8InductionRecursion.lean
@@ -41,5 +41,36 @@ end structural_induction
 
 /- SECTION: Local Recursive Declarations -/
 section the_let_rec
-  --
+  -- NOTE: `let rec` can be used in tactic mode
+  namespace List
+    def replicate'.{u} {α : Type u} (n : Nat) (a : α) : List α :=
+      let rec loop : Nat → List α → List α
+        | 0, as       => as
+        | .succ n, as => loop n (a :: as)
+      ; loop n []
+    theorem length_replicate'.{u}
+      {α : Type u}
+      (n : Nat) (a : α)
+      : (replicate' n a).length = n
+      := by
+        let rec aux
+          (n : Nat)
+          (as : List α)
+          : (replicate'.loop a n as).length = n + as.length
+          := by
+            match n with
+            | 0       => simp [replicate'.loop]
+            | .succ n => simp [replicate'.loop, aux] ; simp_arith
+        exact aux n []
+  end List
+
+  -- NOTE: "postfix" `where` is the same as "prefix" `let rec`.
+  --       Syntax is exactly the same as Haskell.
 end the_let_rec
+
+
+
+/- SECTION: Well-Founded Recursion and Induction -/
+section woof
+  --
+end woof
