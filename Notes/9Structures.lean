da353c3d8490ee495608527f1d2f20d2668c827c
Gabe Field <gabriel.field@uqconnect.edu.au>
2024-12-20 18:36:36 +1000

    shitpost

---
+++ Notes/9Structures.lean
@@ -0,0 +1,16 @@
+/- @file Notes/9Structures.lean
+ - @src https://lean-lang.org/theorem_proving_in_lean4/structures_and_records.html
+ -/
+
+/- SECTION: Declaring Structures -/
+-- What you think
+
+
+
+/- SECTION: Objects -/
+-- What you think
+
+
+
+/- SECTION: Inheritance -/
+-- What you think
